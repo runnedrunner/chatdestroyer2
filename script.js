97e8f68362fbe6921f0e5daccc28240917d74075
runnedrunner <auszhou@nuevaschool.org>
2016-11-02 12:16:17 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var cleanerUser = "toddlerBot"
 var accountColor = "red"
 
 var useUser = (cleanerUser + ": " + message);
-var myVar = setInterval(thing, Math.random() * (10000 - 1000) + 1000);
+var myVar = setInterval(thing, Math.random() * (100000 - 10000) + 10000);
 
 function thing () { 
   addListItem("saylist", cleanerUser +"," + message + "," + accountColor);
