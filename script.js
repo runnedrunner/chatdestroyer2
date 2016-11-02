59b6e247a92d445645cb620dbdac83946c2a95f5
runnedrunner <auszhou@nuevaschool.org>
2016-11-02 09:44:43 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var chatlist = []
 
 var message = 'undefined'
 var cleanerUser = "undefined"
+addListItem("saylist", cleanerUser +"," + antijosh + "," + accountColor);
+
 var useUser = (cleanerUser + ": " + message);
 while (true) { addListItem("saylist", useUser);
 }
