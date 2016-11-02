a2ed2ffab2d9a9947834bc62d7dd3bbf12e6b4ae
runnedrunner <auszhou@nuevaschool.org>
2016-11-02 09:38:27 -0700

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var chatlist = []
+
+// When you press enter ____________________________________________
+
+var message = 'OCANADA!'
+var cleanerUser = "AnonymouseUser"
+var useUser = (cleanerUser.bold().fontcolor("red") + ": ".bold().fontcolor("red") + message)
+addListItem("saylist", useUser);
