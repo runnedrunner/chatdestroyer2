e2adeec850c9ecbc64e42dd202055fc89c52aa56
runnedrunner <auszhou@nuevaschool.org>
2016-11-02 09:47:27 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ var chatlist = []
 
 // When you press enter ____________________________________________
 
-var message = 'copypasta'
-var cleanerUser = "undefined"
+var message = '¸¸♬·¯·♩¸¸♪·¯·♫¸¸Happy Birthday To You¸¸♬·¯·♩¸¸♪·¯·♫¸¸'
+var cleanerUser = "hello"
 var accountColor = "red"
 
 var useUser = (cleanerUser + ": " + message);
-while (true) { addListItem("saylist", cleanerUser +"," + message + "," + accountColor);
-}
+for (var i = 0; i < 100; i++) { addListItem("saylist", cleanerUser +"," + message + "," + accountColor);}
